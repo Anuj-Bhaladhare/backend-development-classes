24eb745c58210ed9b309b61e399d381922e64f29
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-09 13:11:44 +0530

    initialisation class number-03

---
+++ todo-app-class-03/index.js
@@ -0,0 +1,39 @@
+
+
+const express = require("express");
+const app = express();
+
+
+// load config from env File
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+
+
+// middlewear to parser json request body
+app.use(express.json());
+
+
+
+// import routes from Todo API
+const todoRoutes = require("./routes/todos");
+// mount the Todo API Routes
+app.use("api/v1", todoRoutes);
+
+
+
+// start your server
+app.listen( PORT, () => {
+    console.log(`server Started SuccessFully at ${PORT}.....!`);
+})
+
+
+// connection with mongoDB
+const connectDB = require("./config/dataBase");
+connectDB();
+
+
+// defualt routes
+app.get("/", (req, res) => {
+    res.send("<h1>This is your home page</h1>");
+})
